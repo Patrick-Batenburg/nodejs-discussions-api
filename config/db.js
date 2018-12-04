1dadfeec8e17af99e22b066a557d72c5d5ac6774
Patrick <zammypatrick@gmail.com>
2018-12-04 19:33:08 +0100

    Changed mongodb connection

--- config/db.js
+++ config/db.js
@@ -1,3 +1,4 @@
 module.exports = {
-    MONGO_CONNECT_URL: "mongodb://localhost:27017/threadapi"
+    MONGO_CONNECT_URL: "mongodb://admin:YqJVdvGdx6V6Bu4@ds024748.mlab.com:24748/threadapi"    
+    //MONGO_CONNECT_URL: "mongodb://localhost:27017/threadapi"
 };
